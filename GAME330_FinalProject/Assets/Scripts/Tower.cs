56bd4f6f2f1b74a90ba7d8456446a31d553ef150
Clayton Chappell <claytonchappell113@gmail.com>
2018-04-22 21:21:08 -0400

    c

--- GAME330_FinalProject/Assets/Scripts/Tower.cs
+++ GAME330_FinalProject/Assets/Scripts/Tower.cs
@@ -10,6 +10,9 @@ public class Tower : MonoBehaviour {
     public GameObject healthBar;
     public GameObject gameOver;
     public bool gameOverActive;
+    public AudioSource audioSource;
+    public AudioClip explode;
+    public GameObject explosion;
 
     // Use this for initialization
     void Start () {
@@ -20,9 +23,9 @@ public class Tower : MonoBehaviour {
 	void Update () {
         if (health <= 0)
         {
-            Time.timeScale = 0;
-            gameOver.SetActive(true);
-            Destroy(gameObject);
+            Instantiate(explosion, transform.position, transform.rotation);
+            audioSource.PlayOneShot(explode);
+            StartCoroutine(GameOver(5));
         }
         else if (health > 1)
         {
@@ -35,11 +38,20 @@ public class Tower : MonoBehaviour {
         if (other.gameObject.tag == "EnemyR" || other.gameObject.tag == "EnemyG" || other.gameObject.tag == "EnemyB" || other.gameObject.tag == "EnemyY")
         {
             Destroy(other.gameObject);
-            health -= .05f;
+            health -= .1f;
             GetComponent<Animator>().SetTrigger("TookDamage");
             Debug.Log("hit");
             healthBar.transform.localScale = new Vector3(health , healthBar.transform.localScale.y, healthBar.transform.localScale.z);
         }
     }
-    
+
+    private IEnumerator GameOver(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Time.timeScale = 0;
+        gameOver.SetActive(true);
+        Destroy(gameObject);
+    }
+
 }
