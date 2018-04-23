adc607e3e21e816d723bd0a1a76942a7e136368f
krymeariver7 <kvnryan7@gmail.com>
2018-04-23 12:54:18 -0400

    Watch Added

---
+++ GAME330_FinalProject/Assets/Watch/ChangeTextOnInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTextOnInput : MonoBehaviour {
+
+    Text myText;
+
+    // Use this for initialization
+    void Start () {
+        myText = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        string textToPrint = "Pressed Buttons:\n";
+
+        if (SimonXInterface.GetButton(SimonXInterface.SimonButtonType.Button_UL))
+        {
+            textToPrint += "UL ";
+        }
+
+        if (SimonXInterface.GetButton(SimonXInterface.SimonButtonType.Button_UR))
+        {
+            textToPrint += "UR ";
+        }
+
+        if (SimonXInterface.GetButton(SimonXInterface.SimonButtonType.Button_LL))
+        {
+            textToPrint += "LL ";
+        }
+
+        if (SimonXInterface.GetButton(SimonXInterface.SimonButtonType.Button_LR))
+        {
+            textToPrint += "LR";
+        }
+
+        myText.text = textToPrint;
+    }
+}
